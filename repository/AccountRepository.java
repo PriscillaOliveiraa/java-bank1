4efff06e02048a571934dae4d60550c984529ae0
Priscila Oliveira <164383318+PriscillaOliveiraa@users.noreply.github.com>
2025-08-28 15:04:21 -0300

    Update AccountRepository.java
    Essa parte do código é responsável por gerenciar operações relacionadas a contas de carteira, como criação, depósito, saque e transferência. Ela armazena uma lista de contas, cada uma representada pelas classes.

--- repository/AccountRepository.java
+++ repository/AccountRepository.java
@@ -2,12 +2,34 @@ package br.com.dio.model.repository;
 
 import br.com.dio.model.AccountWallet;
 import br.com.dio.model.exception.AcountNotFoundException;
-
 import java.util.List;
 
 public class AccountRepository {
-
     private List<AccountWallet> accounts;
+    public AccountWallet create(final List<String> pix, final long initialFunds){
+        var newAccount = new AccountWallet(initialFunds, pix);
+        return newAccount;
+    }
+
+    public void deposit(final String pix, final long fundsAmount){
+        var target AccountWallet = findByPix(pix);
+        target.addMoney(fundsAmount, descripion: "depósito");
+    }
+
+    public void withdraw(final String pix, final long amount){
+        var source:AccountWallet = findByPix(pix);
+        checkFundsForTransaction(source, amount);
+        source.reduceMoney(amount);
+            return amount;
+    }
+
+    public void transferMoney(final String sourcePix, final String targetPix, final long amount){
+        var source:AccountWallet = findByPix(sourcePix);
+        checkFundsForTransaction(source, amount);
+        var target AccountWallet = findByPix(targetPix);
+        var message = "pix enviado de "+ sourcePix + " para " + targetPix + "";
+        target.addMoney(source.reduceMoney(amount), source.getService(), message);
+    }
 
     public AccountWallet findByPix(final String pix){
         return accounts.stream()
